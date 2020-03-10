ecbd611b3ff6756630172e1f50e835b9b6254e5f
njihiamark <njihiamark@gmail.com>
2020-03-10 20:50:04 +0300

    Redux data & Navigation

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
 import MealList from '../components/MealList';
-import {MEALS} from '../data/dummy-data';
+import { useSelector } from 'react-redux';
+
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 
 const FavoritesScreen = props => {
-  const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2')
+  const favMeals = useSelector(state => state.meals.favoritemeals);
+
   return (
     <MealList listData={favMeals} navigation={props.navigation}/>
   );
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {View, Text, StyleSheet, ScrollView, Image} from 'react-native';
 import { HeaderButtons,  Item} from 'react-navigation-header-buttons';
 
-import {MEALS} from '../data/dummy-data';
+import { useSelector } from 'react-redux';
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
 
@@ -13,9 +13,14 @@ const ListItem = props => {
 };
 
 const MealDetailScreen = props => {
-
+  const availableMeals = useSelector(state => state.meals.meals);
   const mealId = props.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const selectedMeal = availableMeals.find(meal => meal.id === mealId);
+
+  // useEffect(()=>{
+  //   props.navigation.setParams({mealTitle: selectedMeal.title});
+  // }, [selectedMeal]);
+  
 
   return (
     <ScrollView>
@@ -41,9 +46,10 @@ const MealDetailScreen = props => {
 
 MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const mealTitle = navigationData.navigation.getParam('mealTitle');
+  //const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
-    headerTitle: selectedMeal.title,
+    headerTitle: mealTitle,
     headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
       <Item 
         title='Favorite' 
